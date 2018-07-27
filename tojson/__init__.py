9c3272d1682dc7af3e0ea26ca10863eaf00db097
Luca <colleoni@motorialab.com>
2018-07-27 16:27:23 +0200

    Update version number

--- tojson/__init__.py
+++ tojson/__init__.py
@@ -1,4 +1,4 @@
 from decorators import render_to_json, login_required_json, to_json_response
 
-__version__ = "0.3.0"
+__version__ = "0.3.3"
 __all__ = ['render_to_json', 'login_required_json', 'to_json_response']
